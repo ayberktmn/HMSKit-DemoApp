f8b4d60b90a1854a7159b173772f44fd416cc462
ayberktemin <ayberktemin@gmail.com>
2023-09-18 14:58:19 +0300

    receipt auth,silentsign HomeActivity

--- app/src/main/java/com/example/accountkithms/HomeActivity.kt
+++ app/src/main/java/com/example/accountkithms/HomeActivity.kt
@@ -4,21 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import com.example.accountkithms.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val idToken = intent.getStringExtra("idToken")
-        val txt = findViewById<TextView>(R.id.txtIdToken)
+        val SilentSignin = intent.getStringExtra("SilentSignin")
+        val authorizationCode = intent.getStringExtra("authorizationCode")
 
-        if (idToken != null) {
+        if (idToken != null || authorizationCode != null) {
+            binding.txtIdToken.text = idToken.toString()
+            binding.txtSilentSign.text = "Silent Singin ile giris yapiniz!"
 
-            txt.text = idToken.toString()
+            if (authorizationCode == null){
+                binding.txtAuthorization.text = "AuthorizationCode ile giris yapiniz!"
+            } else{
+                binding.txtAuthorization.text = authorizationCode.toString()
+            }
+
+            if (idToken == null){
+                binding.txtIdToken.text = "IdToken ile giris yapiniz!"
+            } else{
+                binding.txtIdToken.text = idToken.toString()
+            }
+
         } else {
             // idToken null ise işleme devam edin veya hata işleme yapın
             Toast.makeText(this,"id token:" + idToken,Toast.LENGTH_SHORT).show()
+        }
+        if (SilentSignin != null) {
+            binding.txtSilentSign.text = SilentSignin.toString()
+            binding.txtAuthorization.text = "AuthorizationCode ile giris yapiniz!"
+            binding.txtIdToken.text = "IdToken ile giris yapiniz!"
+        } else {
+            // idToken null ise işleme devam edin veya hata işleme yapın
+            Toast.makeText(this,"Silent Signin:" + SilentSignin,Toast.LENGTH_SHORT).show()
         }
     }
 }
